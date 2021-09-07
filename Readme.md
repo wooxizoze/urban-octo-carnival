e89fd05ea5c5debfec743dd525ac0788b9ded6d1
Dmitry Krasnov <dzm.krasnov@gmail.com>
2021-09-07 21:32:38 +0300

    Add  file structure

---
+++ Readme.md
@@ -0,0 +1,13 @@
+### Allure
+
+Run this command to generate the allure report in the directory `./test-report/allure-report`:
+
+```bash
+npm run report:generate
+```
+
+You can run this command to start a server on your machine and open the allure report on the browser:
+
+```bash
+npm run report:open
+```
--- wdio.conf.js
+++ wdio.conf.js
@@ -20,7 +20,7 @@ exports.config = {
   // then the current working directory is where your `package.json` resides, so `wdio`
   // will be called from there.
   //
-  specs: ["./test/features/*.feature"],
+  specs: ["./test/features/**/*.feature"],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
