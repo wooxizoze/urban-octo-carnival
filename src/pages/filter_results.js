61ef2024713ca79aab7e4b3b16463af191c7dfdf
Dmitry Krasnov <dzm.krasnov@gmail.com>
2021-09-07 10:53:58 +0300

    Add filter result page

--- src/pages/filter_results.js
+++ src/pages/filter_results.js
@@ -1,4 +1,12 @@
 const Page = require("./page");
-class FilterResults extends Page {}
+class FilterResults extends Page {
+  get filterResultItemImg() {
+    return $(".result__img img");
+  }
+
+  get filterResultItemName() {
+    return $(".j-ga_track.result__link > .result__name");
+  }
+}
 
 module.exports = new FilterResults();
